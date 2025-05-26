712be9e500288a1685a62a3c555251726e4f98d7
kokilavanim@piraiinfo.com <mailid>
2025-05-26 16:21:02 +0530

    Refactor delete_task function and improve user input prompts in main loop

--- src/main.py
+++ src/main.py
@@ -14,22 +14,37 @@ def list_task():
             print(task)
 
 def delete_task():
-    index=int(input("enter the task number to delete"))
+    index=int(input("enter the task number to delete: "))
     if 0 <= index < len(tasks):
         deleted_task=tasks.pop(index)
-        print(f"Deleted task :  {deleted_task['task']}")
+        print(f"Deleted task: {deleted_task['task']}")
 
 
 
 if __name__=="__main__":
-    while True:
-        user_task=input("Enter the task (or type exit): ")
-        if user_task=="exit":
-            break
-        task_priority=input("Enter the priority level: ")
-        add_task(user_task,task_priority)
-        
 
-    list_task()
-    delete_task()
+    while True:
+        print("Task Manager")
+        print("1.Add task")
+        print("2.List task")
+        print("3.Delete task")
+        print("4.exit")
+        
+        choice=input("Enter the choice: ")
+
+        if choice=="1":
+            user_task=input("Enter the task: ")
+            task_priority=input("Enter the priority level: ")
+            add_task(user_task,task_priority)
+
+        elif choice=="2":
+            list_task()
+
+        elif choice=="3":
+            delete_task()
+
+        else:
+            print("exit")
+            break
       
+
